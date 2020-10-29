f72216d69c373fca44bdc2ef1a9bd4173eb654cb
Dan Reardon <dreardon1021@gmail.com>
2020-10-29 13:08:39 -0600

    Add logic to App functions

--- src/App/App.js
+++ src/App/App.js
@@ -9,16 +9,32 @@ class App extends Component {
   }
 
   addToCounter() {
-
+    console.log("test");
+    let addedCounter = this.state.counter + 1;
+    this.setState({ counter: addedCounter });
   }
 
   subtractFromCounter() {
-    
+    let subtractedCounter = this.state.counter - 1;
+    this.setState({ counter: subtractedCounter });
   }
 
   render() {
-    return <h1>Hello!</h1>;
+    return (
+      <div>
+        <p class="counter-count">{this.state.counter}</p>
+        <button onClick={() => this.addToCounter(e)} className="counter-button">
+          Add
+        </button>
+        <button
+          onClick={() => this.subtractFromCounter(e)}
+          className="counter-button"
+        >
+          Subtract
+        </button>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
