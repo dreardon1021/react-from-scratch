c9e58137752776be754b9986394dd246591bbe88
Dan Reardon <dreardon1021@gmail.com>
2020-10-30 11:36:50 -0600

    Add reducer

---
+++ src/reducers/changeCounter.js
@@ -0,0 +1,13 @@
+export const changeCounter = (state = 0, action) => {
+  console.log("reducer");
+  switch (action.type) {
+    case "ADD_COUNTER":
+      let addedState = state++;
+      return addedState;
+    case "SUBTRACT_COUNTER":
+      let subtractedState = state--;
+      return subtractedState;
+    default:
+      return state;
+  }
+};
